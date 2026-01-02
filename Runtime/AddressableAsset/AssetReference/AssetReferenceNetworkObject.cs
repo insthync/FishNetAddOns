fc677f75b3d012c1869bba489d4559da99615ff5
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2026-01-03 01:29:46 +0700

    feat: make `AssetReferenceNetworkObject` serializable

--- Runtime/AddressableAsset/AssetReference/AssetReferenceNetworkObject.cs
+++ Runtime/AddressableAsset/AssetReference/AssetReferenceNetworkObject.cs
@@ -3,6 +3,7 @@ using Insthync.AddressableAssetTools;
 
 namespace FishNet.Insthync.AddressableAsset
 {
+    [System.Serializable]
     public class AssetReferenceNetworkObject : AssetReferenceComponent<NetworkObject>
     {
         public AssetReferenceNetworkObject(string guid) : base(guid)
