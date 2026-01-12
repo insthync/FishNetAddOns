e03eefe9600ee9945c079db6379eb46692af9238
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2026-01-12 18:46:08 +0700

    feat: add functions which its name and parameters like in LiteNetLib's NetDataReader to make it easier to use existing code

--- Runtime/LiteNetLibSerializing/FishNetReaderExtensions.cs
+++ Runtime/LiteNetLibSerializing/FishNetReaderExtensions.cs
@@ -1,11 +1,14 @@
 using FishNet.Serializing;
+using LiteNetLib;
 using LiteNetLib.Utils;
 using System;
+using System.Net;
 
 namespace FishNet.Insthync.LiteNetLibSerializing
 {
     public static class FishNetReaderExtensions
     {
+        #region Functions to read if data type is `INetSerializable`
         // NOTE: I don't think serialization will be called concurrently, but if it is this will need to be changed to a pool or something similar.
         public static readonly NetDataReader LiteNetLibReader = new NetDataReader();
 
@@ -28,5 +31,205 @@ namespace FishNet.Insthync.LiteNetLibSerializing
             LiteNetLibReader.Clear();
             return result;
         }
+        #endregion
+
+        #region Functions which its name and parameters like in LiteNetLib's NetDataReader to make it easier to use existing code
+        public static void Get(this Reader reader, out IPEndPoint result)
+        {
+            result = reader.GetNetEndPoint();
+        }
+
+        public static void Get(this Reader reader, out byte result)
+        {
+            result = reader.ReadUInt8Unpacked();
+        }
+
+        public static void Get(this Reader reader, out sbyte result)
+        {
+            result = reader.ReadInt8Unpacked();
+        }
+
+        public static void Get(this Reader reader, out bool result)
+        {
+            result = reader.ReadBoolean();
+        }
+
+        public static void Get(this Reader reader, out char result)
+        {
+            result = reader.ReadChar();
+        }
+
+        public static void Get(this Reader reader, out ushort result)
+        {
+            result = reader.ReadUInt16();
+        }
+
+        public static void Get(this Reader reader, out short result)
+        {
+            result = reader.ReadInt16();
+        }
+
+        public static void Get(this Reader reader, out ulong result)
+        {
+            result = reader.ReadUInt64();
+        }
+
+        public static void Get(this Reader reader, out long result)
+        {
+            result = reader.ReadInt64();
+        }
+
+        public static void Get(this Reader reader, out uint result)
+        {
+            result = reader.ReadUInt32();
+        }
+
+        public static void Get(this Reader reader, out int result)
+        {
+            result = reader.ReadInt32();
+        }
+
+        public static void Get(this Reader reader, out double result)
+        {
+            result = reader.ReadDouble();
+        }
+
+        public static void Get(this Reader reader, out float result)
+        {
+            result = reader.ReadSingle();
+        }
+
+        public static void Get(this Reader reader, out string result)
+        {
+            result = reader.ReadStringAllocated();
+        }
+
+        public static IPEndPoint GetNetEndPoint(this Reader reader)
+        {
+            string host = reader.ReadStringAllocated();
+            int port = reader.ReadInt32();
+            return NetUtils.MakeEndPoint(host, port);
+        }
+
+        public static byte GetByte(this Reader reader)
+        {
+            return reader.ReadUInt8Unpacked();
+        }
+
+        public static sbyte GetSByte(this Reader reader)
+        {
+            return reader.ReadInt8Unpacked();
+        }
+
+        public static T[] GetArray<T>(this Reader reader) where T : new()
+        {
+            return reader.ReadArrayAllocated<T>();
+        }
+
+        public static bool[] GetBoolArray(this Reader reader)
+        {
+            return reader.ReadArrayAllocated<bool>();
+        }
+
+        public static ushort[] GetUShortArray(this Reader reader)
+        {
+            return reader.ReadArrayAllocated<ushort>();
+        }
+
+        public static short[] GetShortArray(this Reader reader)
+        {
+            return reader.ReadArrayAllocated<short>();
+        }
+
+        public static int[] GetIntArray(this Reader reader)
+        {
+            return reader.ReadArrayAllocated<int>();
+        }
+
+        public static uint[] GetUIntArray(this Reader reader)
+        {
+            return reader.ReadArrayAllocated<uint>();
+        }
+
+        public static float[] GetFloatArray(this Reader reader)
+        {
+            return reader.ReadArrayAllocated<float>();
+        }
+
+        public static double[] GetDoubleArray(this Reader reader)
+        {
+            return reader.ReadArrayAllocated<double>();
+        }
+
+        public static long[] GetLongArray(this Reader reader)
+        {
+            return reader.ReadArrayAllocated<long>();
+        }
+
+        public static ulong[] GetULongArray(this Reader reader)
+        {
+            return reader.ReadArrayAllocated<ulong>();
+        }
+
+        public static string[] GetStringArray(this Reader reader)
+        {
+            return reader.ReadArrayAllocated<string>();
+        }
+
+        public static bool GetBool(this Reader reader)
+        {
+            return reader.ReadBoolean();
+        }
+
+        public static char GetChar(this Reader reader)
+        {
+            return reader.ReadChar();
+        }
+
+        public static ushort GetUShort(this Reader reader)
+        {
+            return reader.ReadUInt16();
+        }
+
+        public static short GetShort(this Reader reader)
+        {
+            return reader.ReadInt16();
+        }
+
+        public static long GetLong(this Reader reader)
+        {
+            return reader.ReadInt64();
+        }
+
+        public static ulong GetULong(this Reader reader)
+        {
+            return reader.ReadUInt64();
+        }
+
+        public static int GetInt(this Reader reader)
+        {
+            return reader.ReadInt32();
+        }
+
+        public static uint GetUInt(this Reader reader)
+        {
+            return reader.ReadUInt32();
+        }
+
+        public static float GetFloat(this Reader reader)
+        {
+            return reader.ReadSingle();
+        }
+
+        public static double GetDouble(this Reader reader)
+        {
+            return reader.ReadDouble();
+        }
+
+        public static string GetString(this Reader reader)
+        {
+            return reader.ReadStringAllocated();
+        }
+        #endregion
     }
 }
--- Runtime/LiteNetLibSerializing/FishNetWriterExtensions.cs
+++ Runtime/LiteNetLibSerializing/FishNetWriterExtensions.cs
@@ -1,10 +1,12 @@
 using FishNet.Serializing;
 using LiteNetLib.Utils;
+using System.Net;
 
 namespace FishNet.Insthync.LiteNetLibSerializing
 {
     public static class FishNetWriterExtensions
     {
+        #region Functions to write if data type is `INetSerializable`
         // NOTE: I don't think serialization will be called concurrently, but if it is this will need to be changed to a pool or something similar.
         public static readonly NetDataWriter LiteNetLibWriter = new NetDataWriter();
 
@@ -15,5 +17,144 @@ namespace FishNet.Insthync.LiteNetLibSerializing
             LiteNetLibWriter.Put(serializable);
             writer.WriteUInt8ArrayAndSize(LiteNetLibWriter.Data, 0, LiteNetLibWriter.Length);
         }
+        #endregion
+
+        #region Functions which its name and parameters like in LiteNetLib's NetDataWriter to make it easier to use existing code
+        public static void Put(this Writer writer, float value)
+        {
+            writer.WriteSingle(value);
+        }
+
+        public static void Put(this Writer writer, double value)
+        {
+            writer.WriteDouble(value);
+        }
+
+        public static void Put(this Writer writer, long value)
+        {
+            writer.WriteInt64(value);
+        }
+
+        public static void Put(this Writer writer, ulong value)
+        {
+            writer.WriteUInt64(value);
+        }
+
+        public static void Put(this Writer writer, int value)
+        {
+            writer.WriteInt32(value);
+        }
+
+        public static void Put(this Writer writer, uint value)
+        {
+            writer.WriteUInt32(value);
+        }
+
+        public static void Put(this Writer writer, char value)
+        {
+            writer.WriteChar(value);
+        }
+
+        public static void Put(this Writer writer, ushort value)
+        {
+            writer.WriteUInt16(value);
+        }
+
+        public static void Put(this Writer writer, short value)
+        {
+            writer.WriteInt16(value);
+        }
+
+        public static void Put(this Writer writer, sbyte value)
+        {
+            writer.WriteInt8Unpacked(value);
+        }
+
+        public static void Put(this Writer writer, byte value)
+        {
+            writer.WriteUInt8Unpacked(value);
+        }
+
+        public static void Put(this Writer writer, byte[] data, int offset, int length)
+        {
+            writer.WriteUInt8ArrayAndSize(data, offset, length);
+        }
+
+        public static void Put(this Writer writer, byte[] data)
+        {
+            writer.WriteUInt8ArrayAndSize(data);
+        }
+
+        public static void Put(this Writer writer, bool value)
+        {
+            writer.WriteBoolean(value);
+        }
+
+        public static void PutArray(this Writer writer, float[] value)
+        {
+            writer.WriteArray(value);
+        }
+
+        public static void PutArray(this Writer writer, double[] value)
+        {
+            writer.WriteArray(value);
+        }
+
+        public static void PutArray(this Writer writer, long[] value)
+        {
+            writer.WriteArray(value);
+        }
+
+        public static void PutArray(this Writer writer, ulong[] value)
+        {
+            writer.WriteArray(value);
+        }
+
+        public static void PutArray(this Writer writer, int[] value)
+        {
+            writer.WriteArray(value);
+        }
+
+        public static void PutArray(this Writer writer, uint[] value)
+        {
+            writer.WriteArray(value);
+        }
+
+        public static void PutArray(this Writer writer, ushort[] value)
+        {
+            writer.WriteArray(value);
+        }
+
+        public static void PutArray(this Writer writer, short[] value)
+        {
+            writer.WriteArray(value);
+        }
+
+        public static void PutArray(this Writer writer, bool[] value)
+        {
+            writer.WriteArray(value);
+        }
+
+        public static void PutArray(this Writer writer, string[] value)
+        {
+            writer.WriteArray(value);
+        }
+
+        public static void PutArray<T>(this Writer writer, T[] value) where T : new()
+        {
+            writer.WriteArray(value);
+        }
+
+        public static void Put(this Writer writer, IPEndPoint endPoint)
+        {
+            writer.WriteString(endPoint.Address.ToString());
+            writer.WriteInt32(endPoint.Port);
+        }
+
+        public static void Put(this Writer writer, string value)
+        {
+            writer.WriteString(value);
+        }
+        #endregion
     }
 }
