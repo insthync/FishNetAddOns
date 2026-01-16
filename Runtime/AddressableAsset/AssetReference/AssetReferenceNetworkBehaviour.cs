8e6a95c0799cf943e992e48d20512c2da49db5f2
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2026-01-16 18:44:54 +0700

    feat: add asset reference by network behaviour

---
+++ Runtime/AddressableAsset/AssetReference/AssetReferenceNetworkBehaviour.cs
@@ -0,0 +1,22 @@
+using FishNet.Object;
+
+namespace FishNet.Insthync.AddressableAsset
+{
+    [System.Serializable]
+    public class AssetReferenceNetworkBehaviour<TBehaviour> : AssetReferenceNetworkObject
+        where TBehaviour : NetworkBehaviour
+    {
+        public AssetReferenceNetworkBehaviour(string guid) : base(guid)
+        {
+        }
+    }
+
+
+    [System.Serializable]
+    public class AssetReferenceNetworkBehaviour : AssetReferenceNetworkBehaviour<NetworkBehaviour>
+    {
+        public AssetReferenceNetworkBehaviour(string guid) : base(guid)
+        {
+        }
+    }
+}
